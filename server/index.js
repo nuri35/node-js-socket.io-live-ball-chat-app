58f974fb6192b780c277a204af3558a4c0ba5326
Nurettin Şen <nurie487@gmail.com>
2022-03-29 14:55:05 +0300

    Add socket server ready

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const app = express()
+const http = require('http');
 const bodyParser = require("body-parser")
 const cors = require("cors")
-
+const { Server } = require("socket.io");
+const server = http.createServer(app);
 require('dotenv').config();
 const cookieParser = require('cookie-parser')
 
 
-
-
-app.use(cors({origin:"http://localhost:3000",credentials:true})) 
+app.use(cors()) 
 
 app.use(cookieParser())
  app.use(bodyParser.json({limit:"50mb",extended:true})) 
@@ -20,13 +20,34 @@ app.use(express.urlencoded({extended:true}))
 
 
 
+const io = new Server(server,{
+
+    cors:{
+        origin:"http://localhost:3000",
+        credentials:true,
+        methods: ["GET","POST","PUT",]
+    }
+
+
+});
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected' + socket.id);
+
+    socket.on("disconnect",()=>{
+        console.log("user disconnect" + socket.id)
+    })
+
+  });
 
 
 
 
-const server = app.listen(process.env.PORT,()=>{
+
+const serverApp = server.listen(process.env.PORT,()=>{
     console.log("bu port dınlenıyor: " + process.env.PORT)
 })
 
 
-module.exports = server
+module.exports = serverApp
