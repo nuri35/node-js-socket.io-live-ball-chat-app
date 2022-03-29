8b2821ec059ec4e171befdc3afe293765b2379c1
Nurettin Şen <nurie487@gmail.com>
2022-03-29 12:43:47 +0300

    Modified

--- client/src/App.js
+++ client/src/App.js
@@ -16,24 +16,15 @@ import {
     Avatar,
     Chip,
     Badge,
-
-  
 } from "@material-ui/core"
 
 
 
-import NewStory from "./components/WriteBlog"
 import Profile from "./components/Profile"
-import Stats from "./components/Stats"
-import Stories from "./components/Stories"
+
 import PostsList from "./components/PostsList"
 import PageNotFound from "./components/PageNotFound"
-import Login from "./components/login"
-import Register from "./components/register"
-import ForgetPassword from "./components/forgetPassword"
-import { AuthContext } from "./components/Context";
-import PostBody from "./components/PostBody"
-import PasswordReset from "./components/PasswordReset"
+
 
 
   const useStyle = makeStyles((theme) => ({
@@ -58,8 +49,6 @@ import PasswordReset from "./components/PasswordReset"
 
 const App = () => {
 
-  const {user,ısAuthenticated,setUser,setIsAuthenticated} = useContext(AuthContext)
-
 
   const classes = useStyle();
 
@@ -81,16 +70,7 @@ const App = () => {
       <Route  path="/" element={<PostsList />} />
       <Route  path="/:tag" element={<PostsList />} />
       <Route   path="verify"  element={<Profile/>}/>
-            <Route   path="User/Profile"  element={<Profile/>}/>
-            <Route   path="/New-story"  element={<NewStory />}/>
-             <Route  path="/User/Stats"  element={<Stats/>}/>
-            <Route  path="/User/Stories"  element={<Stories/>}/>
-            <Route  path="/login"   element={ ısAuthenticated ? <Navigate to="/" /> : <Login />}  />
-            <Route  path="/auth/verify/:activation_token"  element={ ısAuthenticated ? <Navigate to="/" /> : <Register />}/>
-            <Route  path="/register"    element={ ısAuthenticated ? <Navigate to="/" /> : <Register />} />
-            <Route  path="/forget-password"  element={ ısAuthenticated ? <Navigate to="/" /> : <ForgetPassword />}/>
-            <Route  path="/auth/resetPassword/:activatTokenForPass"  element={ ısAuthenticated ? <Navigate to="/" /> : <PasswordReset />}/>
-            <Route   path="/Post/:id"  element={<PostBody />} />
+         
             <Route  path="*" element={<PageNotFound />} />
           
            
