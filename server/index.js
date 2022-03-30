046c217e5ac207eccf3c4a75c677171b2a56a33a
Nurettin Şen <nurie487@gmail.com>
2022-03-30 14:08:58 +0300

    Add socket on receiving

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const app = express()
-const http = require('http');
+
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const { Server } = require("socket.io");
-const server = http.createServer(app);
+
 require('dotenv').config();
 const cookieParser = require('cookie-parser')
 
@@ -20,7 +20,16 @@ app.use(express.urlencoded({extended:true}))
 
 
 
-const io = new Server(server,{
+
+
+
+
+const serverApp = app.listen(process.env.PORT,()=>{
+    console.log("bu port dınlenıyor: " + process.env.PORT)
+})
+
+
+const io = new Server(serverApp,{
 
     cors:{
         origin:process.env.REACT_APP_URI,
@@ -34,13 +43,28 @@ const io = new Server(server,{
 
 io.on('connection', (socket) => {
   
-
     socket.on("join_room", (arg, callback) => {
-        console.log(arg.name + "joined room"); // "world"
-     
-       socket.broadcast.emit("receiving_message", `${arg.name} joined room`);
+        console.log(arg.name + "joined room"); 
+
+        socket.join(arg.roomId);
+      
+    socket.broadcast.emit("receiving_messageUser", arg); 
+    socket.to(arg.roomId).emit("receiving_message", arg);
+  
+        // socket.emit("receiving_messageUser", arg)
+      
       });
 
+
+      socket.on("send_message", (data) => {
+        socket.to(data.room).emit("receive_senderMessage", data);
+      });
+
+      socket.on("leave_room", (data) => {
+        socket.leave(data.room);
+        console.log(data)
+        socket.to(data.room).emit("leave_user",data.author);
+      });
 
 
 
@@ -51,14 +75,6 @@ io.on('connection', (socket) => {
     })
 
   });
-
-
-
-
-
-const serverApp = server.listen(process.env.PORT,()=>{
-    console.log("bu port dınlenıyor: " + process.env.PORT)
-})
 
 
 module.exports = serverApp
